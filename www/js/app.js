7f5476676100018d0190e2a650b97d6c961a8d64
Bengt Weiße <bengt.weisse@flyacts.com>
2015-09-17 14:00:30 +0200

    init commit

---
+++ www/js/app.js
@@ -0,0 +1,196 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins) {
+        if (window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.cordova.plugins) {
+            // then override any default you want
+            window.plugins.nativepagetransitions.globalOptions.duration = 350;
+            window.plugins.nativepagetransitions.globalOptions.iosdelay = 200;
+            window.plugins.nativepagetransitions.globalOptions.androiddelay = 200;
+            window.plugins.nativepagetransitions.globalOptions.winphonedelay = 200;
+            window.plugins.nativepagetransitions.globalOptions.slowdownfactor = 3;
+            // these are used for slide left/right only currently
+            window.plugins.nativepagetransitions.globalOptions.fixedPixelsTop = 0;
+            window.plugins.nativepagetransitions.globalOptions.fixedPixelsBottom = 0;
+        }
+    }
+
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $ionicConfigProvider.views.transition('none');
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+})
+.directive('goNative', ['$ionicGesture', '$ionicPlatform', function($ionicGesture, $ionicPlatform) {
+  return {
+    restrict: 'A',
+
+    link: function(scope, element, attrs) {
+
+      $ionicGesture.on('tap', function(e) {
+
+        var direction = attrs.direction;
+        var transitiontype = attrs.transitiontype;
+
+        $ionicPlatform.ready(function() {
+
+          switch (transitiontype) {
+            case "slide":
+              window.plugins.nativepagetransitions.slide({
+                  "direction": direction
+                },
+                function(msg) {
+                  console.log("success: " + msg)
+                },
+                function(msg) {
+                  alert("error: " + msg)
+                }
+              );
+              break;
+            case "flip":
+              window.plugins.nativepagetransitions.flip({
+                  "direction": direction
+                },
+                function(msg) {
+                  console.log("success: " + msg)
+                },
+                function(msg) {
+                  alert("error: " + msg)
+                }
+              );
+              break;
+
+            case "fade":
+              window.plugins.nativepagetransitions.fade({
+
+                },
+                function(msg) {
+                  console.log("success: " + msg)
+                },
+                function(msg) {
+                  alert("error: " + msg)
+                }
+              );
+              break;
+
+            case "drawer":
+              window.plugins.nativepagetransitions.drawer({
+                  "origin"         : direction,
+                  "action"         : "open"
+                },
+                function(msg) {
+                  console.log("success: " + msg)
+                },
+                function(msg) {
+                  alert("error: " + msg)
+                }
+              );
+              break;
+
+            case "curl":
+              window.plugins.nativepagetransitions.curl({
+                  "direction": direction
+                },
+                function(msg) {
+                  console.log("success: " + msg)
+                },
+                function(msg) {
+                  alert("error: " + msg)
+                }
+              );
+              break;
+
+            default:
+              window.plugins.nativepagetransitions.slide({
+                  "direction": direction
+                },
+                function(msg) {
+                  console.log("success: " + msg)
+                },
+                function(msg) {
+                  alert("error: " + msg)
+                }
+              );
+          }
+
+
+        });
+      }, element);
+    }
+  };
+}]);
